5fa22a85690f362b1ce51dbb9a797b55c18f8623
willymateo <wjmateo@espol.edu.ec>
2024-03-24 14:34:04 -0500

    Add providers section

---
+++ src/services/providers/interfaces.ts
@@ -0,0 +1,33 @@
+import { AuditFields, Order } from "../interfaces";
+
+export interface CreateProviderPayload {
+  phoneNumber: string;
+  firstNames: string;
+  lastNames: string;
+  address: string;
+  idCard: string;
+  email: string;
+}
+
+export interface GetProvidersPayload {
+  orderByField?: string;
+  offset?: number;
+  limit?: number;
+  order?: Order;
+}
+
+export interface GetProvidersResponse {
+  rows: Provider[];
+  count: number;
+}
+
+export interface Provider extends AuditFields {
+  idCard: string | null;
+  email: string | null;
+  phoneNumber: string;
+  firstNames: string;
+  lastNames: string;
+  companyId: string;
+  address: string;
+  id: string;
+}
